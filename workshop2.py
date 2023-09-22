4861d35d4b15ab5acf8e5e6a47dd29452ee65816
JEngHAng <jangg500z@gmail.com>
2023-09-22 12:25:53 +0700

    workshop2

--- workshop2.py
+++ workshop2.py
@@ -5,15 +5,16 @@ def input_data():
     score_01 = float(input("กรอกคะแนนนักเรียนรอบที่ 2 : "))
     score_02 = float(input("กรอกคะแนนนักเรียนรอบที่ 3 : "))
     return id_std , name_std , score_00 , score_01 , score_02
-def calandshow (id_std , name_std , score_00 , score_01 , score_02):
-    score_all = (score_00 + score_01 + score_02) / 3
+def cal (score_00 , score_01 , score_02):
+    score_all = (score_00 + score_01 + score_02) / 3 
+    return score_all
+def show(id_std , name_std , score_00 , score_01 , score_02 , score_all):
     print (f"รหัสนักเรียน : {id_std}")
     print (f"ชื่อนักเรียน : {name_std}")
     print (f"คะแนนลำดับที่ 1 : {score_00} | คะแนนลำดับที่ 2 : {score_01} | คะแนนลำดับที่ 3 : {score_02}")
     print (f"เฉลี่ยทั้ง 3 คะแนนได้ : {score_all:.2f}")
-    return score_all
 print ("***------------------***")
 id_std , name_std , score_00 , score_01 , score_02 = input_data()
 print ("***------------------***")
-calandshow (id_std , name_std , score_00 , score_01 , score_02)
+show(id_std , name_std , score_00 , score_01 , score_02 , cal (score_00 , score_01 , score_02))
 print ("***-------END--------***")
