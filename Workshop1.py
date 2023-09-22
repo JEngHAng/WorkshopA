5bb95b19f31fd87f010d2c5757cd8b84385522de
JEngHAng <jangg500z@gmail.com>
2023-09-22 11:43:23 +0700

    workshop1

---
+++ Workshop1.py
@@ -0,0 +1,22 @@
+def inputdata():
+    product_name = input("Input Product Name : ")
+    product_price = int(input("Input Pirce Product : "))
+    return product_name , product_price
+
+def cal_product_sale ( product_pirce ):
+    product_sale = product_pirce + (product_pirce * 10 / 100)
+    return product_sale
+
+def output_cal_product(product_name , product_pirce , product_sale) :
+    print (f"ชื่อสินค้า {product_name}")
+    print (f"ราคาสินค้า (ต้นทุน) {product_pirce:.2f} บาท")
+    print (f"ราคาขายสินค้า {product_sale:.2f} บาท")
+
+print("-------------------------------------------------")
+print("---------------** PRODUCT SALE **----------------")
+print("-------------------------------------------------")
+product_name , product_pirce = inputdata()
+product_sale = cal_product_sale( product_pirce )
+print("-------------------------------------------------")
+output_cal_product( product_name , product_pirce , product_sale)
+print("-------------------------------------------------")
