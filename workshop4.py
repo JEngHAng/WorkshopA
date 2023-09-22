3bf3c731b9ad78e1f846f072c747e6b23a3a75f6
JEngHAng <jangg500z@gmail.com>
2023-09-22 12:40:37 +0700

    workshop4

--- workshop4.py
+++ workshop4.py
@@ -1,6 +1,16 @@
 def input_data ():
-    x = int(input("Input X : "))
+    x = float(input("Input X : "))
     return x
-def calandshowX (x):
-    y = 2 ( x ** 2 ) + ( 2 * x ) + 1
-    print (f"แก้สมการได้ : {y}")
+def cal(x):
+    y = (( 2 * ( x ** 2 )) + ( 2 * x ) + 1 )
+    return y
+def showx ( x, y ) :
+    print(f'y = ( 2  * ( {x:.0f} ** 2 )) + ( 2 * {x:.0f} ) + 1')
+    print (f"แก้สมการได้ : {y:.0f}")
+
+print ("***------------------***")
+x = input_data()
+print ("***------------------***")
+y = cal(x)
+showx ( x, y )
+print ("***-------END--------***")
