c891975d334e5e7ba4454ec0f0c363116c53d954
JEngHAng <jangg500z@gmail.com>
2023-09-22 12:29:59 +0700

    workshop3

--- workshop3.py
+++ workshop3.py
@@ -2,14 +2,16 @@ def inputdata():
     product_name = input("กรอกชื่อรายการสินค้า : ")
     product_price = int(input("กรอกราคาสินค้า : "))
     return product_name , product_price
-def calandshowproduct( product_name , product_price ):
+def cal( product_price ):
     vat = product_price * 0.07
     chackout = product_price + vat
+    return vat , chackout
+def show( product_name , product_price , vat , chackout):
     print (f"ชื่อรายการสินค้า : {product_name}")
     print (f"ราคาสินค้า : {product_price:.2f} | Vat7% : {vat:.2f} | ราคาที่ต้องชำระ {chackout:.2f}")
-    return vat , chackout
 print ("***------------------***")
 product_name , product_price = inputdata()
 print ("***------------------***")
-calandshowproduct( product_name , product_price )
+vat , chackout = cal( product_price )
+show( product_name , product_price , vat , chackout)
 print ("***-------END--------***")
--- workshop4.py
+++ workshop4.py
@@ -1,1 +1,6 @@
-print (2 ** 3)
+def input_data ():
+    x = int(input("Input X : "))
+    return x
+def calandshowX (x):
+    y = 2 ( x ** 2 ) + ( 2 * x ) + 1
+    print (f"แก้สมการได้ : {y}")
